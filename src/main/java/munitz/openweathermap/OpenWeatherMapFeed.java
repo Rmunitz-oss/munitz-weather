56154e025bfe802b560059b202beb35aa870ee51
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-04-25 17:25:34 -0400

    Weather API query

---
+++ src/main/java/munitz/openweathermap/OpenWeatherMapFeed.java
@@ -0,0 +1,15 @@
+package munitz.openweathermap;
+
+public class OpenWeatherMapFeed {
+    Main main;
+    //Feature features;
+    public static class Main{
+    double temp;
+
+    public double getTemperature(){
+        return temp;
+    }
+
+}
+
+}
