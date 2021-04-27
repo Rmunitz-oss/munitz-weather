96ff0b3a7972585399d51c6e20f15180f459700b
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-04-27 16:44:27 -0400

    Fixed link in OpenWeatherMapService

--- src/main/java/munitz/openweathermap/OpenWeatherMapFeed.java
+++ src/main/java/munitz/openweathermap/OpenWeatherMapFeed.java
@@ -2,14 +2,10 @@ package munitz.openweathermap;
 
 public class OpenWeatherMapFeed {
     Main main;
-    //Feature features;
     public static class Main{
-    double temp;
-
-    public double getTemperature(){
-        return temp;
+        double temp;
+        public double getTemperature(){
+            return temp;
+        }
     }
-
-}
-
 }
